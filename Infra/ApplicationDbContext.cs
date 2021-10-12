d9db68031b7e945644898211539b8510cb1dca6b
Raoul Rocco Riigov <rariig@ttu.ee>
2021-10-12 05:12:33 +0300

    Tegin migratsiooni ka, andmebaasi saab tabeleid luua Data's leiduvatele klassidele. Mitu-mitmele seoste kohad, kus vaja, tuleb veel täpsustada ja lisada.

--- Infra/ApplicationDbContext.cs
+++ Infra/ApplicationDbContext.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using BankingApp.Data;
+using BankingApp.Data.Common;
+using BankingApp.Data.Investing;
+using BankingApp.Data.Loan;
 using Data;
 
 namespace BankingApp.Infra
@@ -17,12 +14,45 @@ namespace BankingApp.Infra
             : base(options)
         {
         }
+        public DbSet<ATMData> ATM { get; set; }
+        public DbSet<ATMManagerData> ATMManager { get; set; }
+        public DbSet<AccountData> Account { get; set; }
+        public DbSet<InvestingAccountData> InvestingAccount { get; set; }
+        public DbSet<InvestmentData> Investment { get; set; }
+        public DbSet<LoanAccountData> LoanAccount { get; set; }
+        public DbSet<CarLoanData> CarLoan { get; set; }
+        public DbSet<HomeLoanData> HomeLoan { get; set; }
+        public DbSet<PersonalLoanData> PersonalLoan { get; set; }
+        public DbSet<LoanManagerData> LoanManager { get; set; }
+        public DbSet<BankData> Bank { get; set; }
+        public DbSet<CustomerData> Customer { get; set; }
+        public DbSet<NotificationData> Notification { get; set; }
+        public DbSet<TransactionData> Transaction { get; set; }
         public DbSet<CalculatorData> Calculator { get; set; }
+
+        //TODO vaadata, kuidas krüptod/stockid listina siia saada. Võib panna käsitsi nt 5 populaarseimat ja listist saab investeerides valida nende vahel.
+        //public DbSet<CryptoData> Crypto { get; set; }
+        //public DbSet<StockData> Stock { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //modelBuilder.Entity<Calculator>().HasKey(m => m.YieldType);
             modelBuilder.Entity<CalculatorData>().ToTable("Calculator");
+            modelBuilder.Entity<ATMData>().ToTable("ATM");
+            modelBuilder.Entity<ATMManagerData>().ToTable("ATMManager");
+            modelBuilder.Entity<AccountData>().ToTable("Account");
+            modelBuilder.Entity<InvestingAccountData>().ToTable("InvestingAccount");
+            modelBuilder.Entity<InvestmentData>().ToTable("Investment");
+            modelBuilder.Entity<LoanAccountData>().ToTable("LoanAccount");
+            modelBuilder.Entity<CarLoanData>().ToTable("CarLoan");
+            modelBuilder.Entity<HomeLoanData>().ToTable("HomeLoan");
+            modelBuilder.Entity<PersonalLoanData>().ToTable("PersonalLoan");
+            modelBuilder.Entity<LoanManagerData>().ToTable("LoanManager");
+            modelBuilder.Entity<BankData>().ToTable("Bank");
+            modelBuilder.Entity<CustomerData>().ToTable("Customer");
+            modelBuilder.Entity<NotificationData>().ToTable("Notification");
+            modelBuilder.Entity<TransactionData>().ToTable("Transaction");
             
+
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ Infra/Migrations/20211012020847_new.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankingApp.Infra.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccountAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccountLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ATM",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Manager = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ATMManager",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FirstMidName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATMManager", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bank",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bank", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarLoan",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CarType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarValue = table.Column<double>(type: "float", nullable: false),
+                    CarAge = table.Column<int>(type: "int", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false),
+                    LoanPeriod = table.Column<int>(type: "int", nullable: false),
+                    Interest = table.Column<double>(type: "float", nullable: false),
+                    LoanManagerId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarLoan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccountId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FirstMidName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HomeLoan",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HomeValue = table.Column<double>(type: "float", nullable: false),
+                    HomeAge = table.Column<int>(type: "int", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false),
+                    LoanPeriod = table.Column<int>(type: "int", nullable: false),
+                    Interest = table.Column<double>(type: "float", nullable: false),
+                    LoanManagerId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HomeLoan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investment",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CurrentAmount = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoanManager",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FirstMidName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanManager", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalLoan",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false),
+                    LoanPeriod = table.Column<int>(type: "int", nullable: false),
+                    Interest = table.Column<double>(type: "float", nullable: false),
+                    LoanManagerId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalLoan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RecipientId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecipientName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SenderId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SenderName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TransactionNr = table.Column<int>(type: "int", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    MoneyAmount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvestingAccount",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvestingAccount", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InvestingAccount_Account_Id",
+                        column: x => x.Id,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoanAccount",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanAccount", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LoanAccount_Account_Id",
+                        column: x => x.Id,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ATM");
+
+            migrationBuilder.DropTable(
+                name: "ATMManager");
+
+            migrationBuilder.DropTable(
+                name: "Bank");
+
+            migrationBuilder.DropTable(
+                name: "CarLoan");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "HomeLoan");
+
+            migrationBuilder.DropTable(
+                name: "InvestingAccount");
+
+            migrationBuilder.DropTable(
+                name: "Investment");
+
+            migrationBuilder.DropTable(
+                name: "LoanAccount");
+
+            migrationBuilder.DropTable(
+                name: "LoanManager");
+
+            migrationBuilder.DropTable(
+                name: "Notification");
+
+            migrationBuilder.DropTable(
+                name: "PersonalLoan");
+
+            migrationBuilder.DropTable(
+                name: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
