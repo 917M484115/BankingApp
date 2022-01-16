cff0dbaf5b03d1a0272000a9f9d83325ac3c7f71
Nikolai Ogonkov <niogon@ttu.ee>
2022-01-16 19:58:53 +0200

    fixed a bug

--- Facade/Investing/OrderView.cs
+++ Facade/Investing/OrderView.cs
@@ -6,7 +6,7 @@ namespace BankingApp.Facade.Investing
     public sealed class OrderView : UniqueEntityView
     {
         [DisplayName("Customer")] public string CustomerID { get;set;}
-        [DisplayName("Order date")] public DateTime OrderDate { get;set;}
+        [DisplayName("Started Trading")] public DateTime OrderDate { get;set;}
         [DisplayName("Customer Name")] public string CustomerName { get;set;}
         [DisplayName("Total price")] public decimal TotalPrice { get;set;}
         public bool Closed { get;set;}
--- Pages/Investing/CryptoBasketItems/CryptoBasketItemsBasePage.cs
+++ Pages/Investing/CryptoBasketItems/CryptoBasketItemsBasePage.cs
@@ -21,15 +21,26 @@ namespace BankingApp.Pages.Investing
     {
         public IEnumerable<SelectListItem> Crypto { get; }
         public IEnumerable<SelectListItem> CryptoBaskets { get; }
-        protected CryptoBasketItemsBasePage(ICryptoBasketItemsRepository r, ICryptoBasketsRepository b, ICryptoRepository p)
+        public ICryptoRepository CryptoRepository { get;}
+        public IEnumerable<SelectListItem> BlockChainsRepository { get;}
+        protected CryptoBasketItemsBasePage(ICryptoBasketItemsRepository r, ICryptoBasketsRepository b, ICryptoRepository p,IBlockChainsRepository bcr)
            : base(r, "Crypto Basket items")
         {
+            CryptoRepository = p;
+            BlockChainsRepository = newItemsList<BlockChain,BlockChainData>(bcr);
             Crypto = newItemsList<Crypto, CryptoData>(p);
             CryptoBaskets = newItemsList<CryptoBasket, CryptoBasketData>(b, null, d => new CryptoBasket(d).Name);
+        }
+        public object GetCryptoByID(string id)=>CryptoRepository.GetById(id);
+        public string GetBlockChainByID(string id)
+        {
+            Crypto c = (Crypto)GetCryptoByID(id);
+            return c.BlockChainID;
         }
         protected internal override string pageSubtitle() => CryptoBasketsName(FixedValue);
         public string CryptoBasketsName(string id) => itemName(CryptoBaskets, id);
         public string CryptoName(string id) => itemName(Crypto, id);
+        public string BlockChainName(string id) => itemName(BlockChainsRepository,GetBlockChainByID(id));
         protected internal override Uri pageUrl() => new Uri("/Manager/CryptoBasketItems", UriKind.Relative);
         protected internal override CryptoBasketItem toObject(CryptoBasketItemView v) => new CryptoBasketItemViewFactory().Create(v);
         protected internal override CryptoBasketItemView toView(CryptoBasketItem o) => new CryptoBasketItemViewFactory().Create(o);
@@ -40,7 +51,6 @@ namespace BankingApp.Pages.Investing
             createColumn(x => Item.CryptoID);
             createColumn(x => Item.CryptoName);
             createColumn(x => Item.Ticker);
-            createColumn(x => Item.BlockChain);
             createColumn(x => Item.UnitPrice);
             createColumn(x => Item.Quantity);
             createColumn(x => Item.TotalPrice);
@@ -49,17 +59,17 @@ namespace BankingApp.Pages.Investing
         }
         public override string GetName(IHtmlHelper<TPage> h, int i) => i switch
         {
-            6 or 8 => getName<decimal>(h, i),
-            7 => getName<int>(h, i),
-            9 or 10 => getName<DateTime?>(h, i),
+            5 or 7 => getName<decimal>(h, i),
+            6 => getName<int>(h, i),
+            8 or 9 => getName<DateTime?>(h, i),
             _ => base.GetName(h, i)
         };
 
         public override IHtmlContent GetValue(IHtmlHelper<TPage> h, int i) => i switch
         {
-            6 or 8 => getValue<decimal>(h, i),
-            7 => getValue<int>(h, i),
-            9 or 10 => getValue<DateTime?>(h, i),
+            5 or 7 => getValue<decimal>(h, i),
+            6 => getValue<int>(h, i),
+            8 or 9 => getValue<DateTime?>(h, i),
             _ => base.GetValue(h, i)
         };
         public override string BackToMasterDetailPageUrl => $"/Manager/CryptoBaskets/Details" +
