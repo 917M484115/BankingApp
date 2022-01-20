705f6508855e2e781489f1c115be771afe201f6c
Raoul Rocco Riigov <rariig@ttu.ee>
2022-01-20 10:26:12 +0200

    .

--- Infra/Loan/LoanManagerRepository.cs
+++ Infra/Loan/LoanManagerRepository.cs
@@ -5,7 +5,7 @@ using BankingApp.Infra.Common;
 namespace BankingApp.Infra.Loan
 {
 	public sealed class LoanManagerRepository :
-		UniqueEntitiesRepository<LoanManager, LoanManagerData>, ILoanManagerRepository
+		UniqueEntitiesRepository<LoanManager, LoanManagerData>, ILoanManagerRepository 
 	{
 		public LoanManagerRepository(ApplicationDbContext c) : base(c, c.LoanManagers) { }
 		protected internal override LoanManager toDomainObject(LoanManagerData d)
---
+++ TestsA/Domain/Loans/CarLoanTests.cs
@@ -0,0 +1,30 @@
+﻿//using BankingApp.Aids.Random;
+//using BankingApp.Data.Loan;
+//using BankingApp.Domain.Loans;
+//using BankingApp.Tests;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace Tests.Domain.Loans
+//{
+//    [TestClass]
+//    public class CarLoanTests : SealedClassTests<LoanData>
+//    {
+//        private CarLoanData data;
+
+//        protected override object createObject() => new CarLoan(data);
+
+//        [TestMethod] public void CarValueTest() => isProperty(data.CarValue);
+//        [TestMethod] public void CarAgeTest() => isProperty(data.CarAge);
+//        [TestMethod] public void CarTypeTest() => isProperty(data.CarType);
+
+//        //[TestMethod]
+//        //public void MonthlyReturnTest()
+//        //{
+//        //    data = GetRandom.Object<CarLoanData>();
+//        //    var expected = 0;
+//        //    areEqual(expected, );
+            
+//        //    areEqual(expected, basket.TotalPrice);
+//        //}
+//    }
+//}
