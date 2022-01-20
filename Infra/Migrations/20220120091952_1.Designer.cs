22aaecd534a525e5465972a925f641dfdb208420
Nikolai Ogonkov <niogon@ttu.ee>
2022-01-20 13:52:06 +0200

    .

--- Infra/Migrations/20220120091952_1.Designer.cs
+++ Infra/Migrations/20220120091952_1.Designer.cs
@@ -4,14 +4,16 @@ using BankingApp.Infra;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankingApp.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220120091952_1")]
+    partial class _1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infra/Migrations/20220120091952_1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankingApp.Infra.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientName",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "SenderId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "SenderName",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ATMProcessId",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencySwapId",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvestmentId",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LoanId",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionId",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ATMProcessId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "CurrencySwapId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "InvestmentId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "LoanId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientId",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientName",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderId",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderName",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
